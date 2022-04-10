8988ded218c4b91622862a02362bef834273317a
lienux <lienux.qq@gmail.com>
2022-04-10 13:02:48 +0700

    add badge

--- resources/views/pages/portofolio.blade.php
+++ resources/views/pages/portofolio.blade.php
@@ -4,70 +4,119 @@
     <div class="alert alert-secondary" role="alert">
         <div class="subheading">Other</div>
         <ul class="fa-ul mb-0">
+            <!-- <li>
+                <a href="#" class="btn btn-sm btn-primary position-relative">
+                    Inbox
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        99+
+                        <span class="visually-hidden">unread messages</span>
+                    </span>
+                </a>
+            </li> -->
+            <li>
+                <span class="fa-li"><i class="fas fa-check"></i></span>
+                Sistem Informasi Akademik
+            </li>
+            <li>
+                <span class="fa-li"><i class="fas fa-check"></i></span>
+                Lawhuis | <a href="http://lawhuis.com" target="_balck">lawhuis.com</a>
+            </li>
+            <li>
+                <span class="fa-li"><i class="fas fa-check"></i></span>
+                Trontrack | <a href="http://trontrack.id" target="_balck">trontrack.id</a>
+            </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
                 Payment Integration with Kaspro, Finnet
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                SMS Gateway
+                MyOrganda <span class="badge bg-success text-light">[ TRON / Organisasi Angkutan Darat ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Inbound Voice
+                SMS Gateway <span class="badge bg-success text-light">[ Digital Orbit Technology ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Road Management System
+                Inbound Voice <span class="badge bg-success text-light">[ Ivosight / Finnet ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                GPS Tracking
-            </li>     
-            <li>
-                <span class="fa-li"><i class="fas fa-check"></i></span>
-                Interaktif Learning System (Media Pembelajaran Online)
+                Road Management System <span class="badge bg-success text-light">[ Digital Orbit Technology ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Sistem Informasi Pendidikan (SIMPEND)
+                GPS Tracking <span class="badge bg-success text-light">[ Digital Orbit Technology ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Absensi Online (Android)
+                Sistem Informasi Pendidikan (SIMPEND) <span class="badge bg-success text-light">[ Digital Orbit Technology ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Booking Hotel (Web + Mobile)
+                Absensi Online (Android) <span class="badge bg-success text-light">[ Digital Orbit Technology ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Document Manajement System
+                Booking Hotel (Web + Mobile) <span class="badge bg-success text-light">[ Digital Orbit Technology ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Fleet Manajement System
+                Document Manajement System <span class="badge bg-success text-light">[ Digital Orbit Technology ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                GPS Logger
+                Fleet Manajement System <span class="badge bg-success text-light">[ Kementrian Perhubungan - Direktorat Angkutan Jalan ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Portal Integrasi GPS
+                GPS Logger <span class="badge bg-success text-light">[ Kementrian Perhubungan - Direktorat Angkutan Jalan ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Ekontrak Angkutan Perintis
+                Portal Integrasi GPS <span class="badge bg-success text-light">[ Kementrian Perhubungan - Direktorat Angkutan Jalan ]</span>
             </li>
             <li>
                 <span class="fa-li"><i class="fas fa-check"></i></span>
-                Aplikasi Data Keuangan Santri
+                Ekontrak Angkutan Perintis <span class="badge bg-success text-light">[ Kementrian Perhubungan - Direktorat Angkutan Jalan ]</span>
+            </li>
+            <li>
+                <span class="fa-li"><i class="fas fa-check"></i></span>
+                Interaktif Learning System (Media Pembelajaran Online) <span class="badge bg-success text-light">[ Pondok Pesantren Umar bin Khotob Plus ]</span>
+            </li>
+            <li>
+                <span class="fa-li"><i class="fas fa-check"></i></span>
+                Aplikasi Data Keuangan Santri <span class="badge bg-success text-light">[ Pondok Pesantren Umar bin Khotob Plus ]</span>
             </li>
         </ul>
     </div>
 
-    <div class="card p-4 mb-4 bg-secondary text-light">                                      
+    <div class="card p-4 mb-4 bg-secondary text-light">
+        <div class="mb-2"> Lawhuis</div>
+
+        <div class="row">
+            <div class="col-xl-4 col-md-4">
+                <div class="card mb-2">
+                    <img src="img/lawhuis/web.png" class="card-img-top" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card p-4 mb-4 bg-secondary text-light">
+        <div class="mb-2"> Trontrack</div>
+
+        <div class="row">
+            <div class="col-xl-4 col-md-4">
+                <div class="card mb-2">
+                    <img src="img/trontrack/map.png" class="card-img-top" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card p-4 mb-4 bg-secondary text-light">
         <div class="mb-2"> Fleet Manajement System</div>
 
         <div class="row">
@@ -78,9 +127,9 @@
             </div>
         </div>
     </div>
-    
 
-    <div class="card p-4 mb-4 bg-secondary text-light">                                      
+
+    <div class="card p-4 mb-4 bg-secondary text-light">
         <div class="mb-2"> GPS Logger | Angkutan Perintis</div>
 
         <div class="row">
@@ -146,7 +195,7 @@
 
         </div>
     </div>
-        
+
     <div class="card p-4 mb-4 bg-secondary text-light">
         <div class="mb-2">E-kontrak | Angkutan Perintis</div>
 
@@ -174,10 +223,10 @@
                     <img src="img/ekontrak/dashboard_bptd.png" class="card-img-top" alt="...">
                 </div>
             </div>
-        </div>        
+        </div>
     </div>
 
-    <div class="card p-4 mb-4 bg-secondary text-light"> 
+    <div class="card p-4 mb-4 bg-secondary text-light">
         <div class="mb-2">Aplikasi Data Keuangan Santri</div>
 
         <div class="row">
@@ -198,10 +247,10 @@
                     <img src="img/dakusa/list_nasabah.png" class="card-img-top" alt="...">
                 </div>
             </div>
-        </div>        
+        </div>
     </div>
 
-    <div class="card p-4 mb-4 bg-secondary text-light">                                      
+    <div class="card p-4 mb-4 bg-secondary text-light">
         <div class="mb-2">
             Dashboard Firepot
         </div>
@@ -230,10 +279,10 @@
                     <img src="img/firepot/api.png" class="card-img-top" alt="...">
                 </div>
             </div> --}}
-        </div>       
+        </div>
     </div>
 
-    <div class="card p-4 mb-4 bg-secondary text-light">                                      
+    <div class="card p-4 mb-4 bg-secondary text-light">
         <div class="mb-2">
             Gen Florist
         </div>
